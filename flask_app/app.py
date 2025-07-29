47346784f9fd6513be568db3a81348f4ab949270
Gokul-Vignesh <gokulvignesh072@gmail.com>
2025-07-29 11:27:19 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template, request
+from src.predict import predict_image
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'flask_app/static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    prediction = None
+    if request.method == "POST":
+        file = request.files["image"]
+        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(filepath)
+        prediction = predict_image(filepath)
+    return render_template("index.html", prediction=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
